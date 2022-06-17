d864628a51b96c4470caa92bcc94a076dcb2f917
cs-hashiguchi <yuka.hashiguchi@central-soft.co.jp>
2022-06-17 13:16:44 +0900

    わーい

---
+++ src/main/java/bean/CelebrationBean.java
@@ -0,0 +1,16 @@
+package bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CelebrationBean {
+	private List<String> PicturePaths = new ArrayList<String>();
+
+	public String getPicturePath(int index) {
+		return PicturePaths.get(index);
+	}
+
+	public void addPicturePath(String picturePath) {
+		PicturePaths.add(picturePath);
+	}
+}
